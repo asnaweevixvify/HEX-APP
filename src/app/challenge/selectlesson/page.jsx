71558566321ff17770b6607bbae9a66e6187f001
asnaweevixvify <asnaweeezor@gmail.com>
2025-08-31 23:31:03 +0700

    game

--- src/app/challenge/selectlesson/page.jsx
+++ src/app/challenge/selectlesson/page.jsx
@@ -1,13 +1,16 @@
 "use client"
 import Image from "next/image"
+import Link from "next/link"
 
 export default function page() {
   return (
     <div className="flex justify-center items-center h-[100vh] flex-wrap gap-10">
-      <div className="flex flex-col justify-around items-center shadow-[0px_4px_6px_7px_rgba(0,_0,_0,_0.1)] rounded-2xl p-5 h-[400px]">
-        <h1 className="text-2xl text-black">FUNCTION</h1>
-        <Image src={"/function.jpg"} width={300} height={300} alt="lesson"></Image>
-      </div>
+      <Link href={"/challenge/game/round1"}>
+        <div className="flex flex-col justify-around items-center shadow-[0px_4px_6px_7px_rgba(0,_0,_0,_0.1)] rounded-2xl p-5 h-[400px]">
+          <h1 className="text-2xl text-black">FUNCTION</h1>
+          <Image src={"/function.jpg"} width={300} height={300} alt="lesson"></Image>
+        </div>
+      </Link>
       <div className="flex flex-col justify-around items-center shadow-[0px_4px_6px_7px_rgba(0,_0,_0,_0.1)] rounded-2xl p-5 h-[400px]">
         <h1 className="text-2xl text-black">MATRIX</h1>
         <Image src={"/matrix.png"} width={300} height={300} alt="lesson"></Image>
